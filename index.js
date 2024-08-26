44895a2e982e1d0fcbbaf36aa994786cee09ae04
Snow Yáng <snow.yang@fluv.com>
2024-08-26 20:52:48 +0800

    Add render

--- index.js
+++ index.js
@@ -20,14 +20,40 @@ function createTextElement(text) {
   };
 }
 
+function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  for (const child of element.props.children) {
+    render(child, dom);
+  }
+
+  container.appendChild(dom);
+  console.log(container);
+}
+
 const Act = {
   createElement,
+  render,
 };
 
 /** @jsx Didact.createElement */
 const element = Act.createElement(
   "div",
-  { id: "Hokkaido" },
-  Act.createElement("p", null, "Tokyo"),
-  Act.createElement("b")
+  { id: "Hokkaido", class: "display: flex" },
+  Act.createElement("p", {id: "osaka"}, "Tokyo"),
+
+  Act.createElement("p", {id: "okinawa"}, Act.createElement("p", null, "Hokkaido"))
 );
+
+const container = document.querySelector("#root");
+Act.render(element, container);
